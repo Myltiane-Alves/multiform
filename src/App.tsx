c00cdd29717f6617ffd494e8397a73b2b3767434
Myltiane Alves <myltiane.aux@gmail.com>
2021-10-09 10:38:21 -0300

    iniciando arquivos

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Router} from './router';
+//import { FormProvider} from './context/FormContext';
+import './App.css';
+
+const App = () => {
+  return (
+    //<FormProvider>
+      <Router />
+   // </FormProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+
+export const Header = () => {
+    return (
+        <C.container>
+            <h1>Cadastro do Desenvolvedor</h1>
+            <p> Faça seu cadastro na lista e receba um emprego intantaneamente.</p>
+        </C.container>
+    )
+}
